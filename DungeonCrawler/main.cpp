df041716474b521009d3799f61db588f26090140
Samuel Hands <sam@samuels-mbp.fritz.box>
2013-12-07 11:40:23 +1100

    Initial commit

--- DungeonCrawler/main.cpp
+++ DungeonCrawler/main.cpp
@@ -7,12 +7,24 @@
 //
 
 #include <iostream>
+#include <SDL2/SDL.h>
 
 int main(int argc, const char * argv[])
 {
+    SDL_Init(SDL_INIT_VIDEO);
+    
+    SDL_Window* window = SDL_CreateWindow("Dungeon Crawler", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_OPENGL);
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    if (window == nullptr) {
+        std::cout << "Could not init Window " << SDL_GetError();
+        return 1;
+    }
+    
+    SDL_Delay(3000);
+    
+    SDL_DestroyWindow(window);
+    
+    SDL_Quit();
     return 0;
 }
 
