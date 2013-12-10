86a87f9a1acff7d89b8d450b73e3e8991af22459
sam <forsaken.panda@gmail.com>
2013-12-10 23:42:54 +1100

    Progress... For real this time

--- DungeonCrawler/RayCastBitmap.cpp
+++ DungeonCrawler/RayCastBitmap.cpp
@@ -15,15 +15,15 @@ Bitmap(height, width)
     //Row based, iterate last number in inner loop for caching
     //XZ Coordinate top down
 
-    // XXX  X
-    //      X
-    //      X
-    // O
+    //   X
+    //   ^
+    //   |
+    //   O
     level[0][0] = 1;
     level[1][0] = 1;
     level[2][0] = 1;
-    level[3][0] = 0;
-    level[4][0] = 0;
+    level[3][0] = 1;
+    level[4][0] = 1;
     level[5][0] = 1;
 
     level[0][1] = 0;
@@ -31,14 +31,14 @@ Bitmap(height, width)
     level[2][1] = 0;
     level[3][1] = 0;
     level[4][1] = 0;
-    level[5][1] = 1;
+    level[5][1] = 0;
 
     level[0][2] = 0;
     level[1][2] = 0;
     level[2][2] = 0;
     level[3][2] = 0;
     level[4][2] = 0;
-    level[5][2] = 1;
+    level[5][2] = 0;
 
     level[0][3] = 0;
     level[1][3] = 0;
@@ -48,9 +48,9 @@ Bitmap(height, width)
     level[5][3] = 0;
 
     posY = 32;
-    posX = 1;
-    posZ = 86;
-    yaw = 0.785398163f;
+    posX = 191;
+    posZ = 250;
+    yaw = 1.57079633f;
 
     mapHeight = 4;
     mapWidth = 6;
@@ -58,10 +58,13 @@ Bitmap(height, width)
 
 void RayCastBitmap::Draw(Game const game)
 {
-    yaw += 0.001;
+    for (int i = 0; i < height * width; i++)
+    {
+        pixels[i] = 0;
+    }
     float angle = yaw - VIEWING_ANGLE / 2;
 
-    for (int i = 0; i < width; i++)
+    for (int i = 0; i < width; i++, angle += ANGLE_BETWEEN_RAYS)
     {
         float hX, hZ, vX, vZ;
         CheckHorizontalIntersections(angle, hX, hZ);
@@ -70,7 +73,10 @@ void RayCastBitmap::Draw(Game const game)
         float hDistance = fabsf(posX - hX) / cosf(angle);
         float vDistance = fabsf(posX - vX) / cosf(angle);
 
-        if (hDistance == INFINITY && INFINITY == vDistance) continue;
+        if (hDistance == INFINITY && INFINITY == vDistance)
+        {
+            continue;
+        }
 
         float distance;
         if (hDistance < vDistance)
@@ -90,15 +96,13 @@ void RayCastBitmap::Draw(Game const game)
         {
             pixels[i + start * width] = 0x00FF00;
         }
-
-        angle += ANGLE_BETWEEN_RAYS;
     }
 }
 
 void RayCastBitmap::CheckHorizontalIntersections(const float angle, float &x, float &z)
 {
     float aZ = floorf(posZ / 64) * 64 + (angle > M_PI ? +64 : -1);
-    float aX = posX + (posZ - aZ) / tanf(angle); //o / t
+    float aX = posX + (posZ - aZ) / tanf(angle);
 
     int zIndex = static_cast<int>(aZ) / 64,
             xIndex = static_cast<int>(aX) / 64;
@@ -109,7 +113,7 @@ void RayCastBitmap::CheckHorizontalIntersections(const float angle, float &x, fl
     while (zIndex < mapHeight && xIndex < mapWidth &&
             zIndex > -1 && xIndex > -1)
     {
-        if (level[zIndex][xIndex] > 0)
+        if (level[xIndex][zIndex] > 0)
         {
             x = aX;
             z = aZ;
@@ -138,7 +142,7 @@ void RayCastBitmap::CheckVerticalIntersections(const float angle, float &x, floa
     while (zIndex < mapHeight && xIndex < mapWidth &&
             zIndex > -1 && xIndex > -1)
     {
-        if (level[zIndex][xIndex] > 0)
+        if (level[xIndex][zIndex] > 0)
         {
             x = aX;
             z = aZ;
