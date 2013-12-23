f554c790a6b3446d813c9201f82465a70b2ad672
sam <forsaken.panda@gmail.com>
2013-12-23 11:08:03 +1100

    Progress

---
+++ DungeonCrawler/Enemy.cpp
@@ -0,0 +1,19 @@
+//
+//  Enemy.cpp
+//  DungeonCrawler
+//
+//  Created by Samuel Hands on 23/12/2013.
+//  Copyright (c) 2013 Sam. All rights reserved.
+//
+
+#include "Enemy.h"
+
+Enemy::Enemy()
+{
+
+}
+
+Enemy::~Enemy()
+{
+
+}
---
+++ DungeonCrawler/Enemy.h
@@ -0,0 +1,22 @@
+//
+//  Enemy.h
+//  DungeonCrawler
+//
+//  Created by Samuel Hands on 23/12/2013.
+//  Copyright (c) 2013 Sam. All rights reserved.
+//
+#ifndef __DungeonCrawler__Enemy__
+#define __DungeonCrawler__Enemy__
+
+#include "Entity.h"
+
+class Enemy : public Entity
+{
+public:
+    Enemy();
+
+    ~Enemy();
+
+};
+
+#endif /* defined(__DungeonCrawler__Enemy__) */
